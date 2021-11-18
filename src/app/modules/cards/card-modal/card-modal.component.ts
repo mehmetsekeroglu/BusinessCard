f5c504f867c029600671a321e63ac28f5df4379f
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-11-18 08:04:33 +0100

    card modal component was added

--- src/app/modules/cards/card-modal/card-modal.component.ts
+++ src/app/modules/cards/card-modal/card-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-card-modal',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    
+  ) { }
 
   ngOnInit(): void {
+
   }
+ 
 
 }
--- src/app/modules/cards/cards.component.ts
+++ src/app/modules/cards/cards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { CardModalComponent } from './card-modal/card-modal.component';
 
 @Component({
   selector: 'app-cards',
@@ -22,4 +23,7 @@ export class CardsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openCardModal(){
+    this.dialog.open(CardModalComponent)
+  }
 }
